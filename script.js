97685761d7fafa28ac502bb2e4db1aaec3125583
cafe51 <cafecafe51@hotmail.com>
2022-02-20 18:52:50 -0400

    requisitos 7 e 8 resolvidos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function adcionarTarefas() {
   const listaTarefa = document.querySelector('#lista-tarefas');
   const inputTarefa = document.querySelector('#texto-tarefa');
   const tarefaNova = document.createElement('li');
+  tarefaNova.setAttribute('class', 'tarefa');
   tarefaNova.innerText = inputTarefa.value;
   listaTarefa.appendChild(tarefaNova);
   inputTarefa.value = '';
@@ -12,4 +13,23 @@ function efeitoBotaoAdicinar() {
   botaoAdcionar.addEventListener('click', adcionarTarefas);
 }
 
+function listaCinza(evt) {
+  const alvo = evt.target;
+  const listaTarefa = document.querySelector('#lista-tarefas');
+  const itens = listaTarefa.children;
+  for (let i = 0; i < itens.length; i += 1) {
+    itens[i].style.backgroundColor = '';
+  }
+  if (alvo.className === 'tarefa') {
+    alvo.style.backgroundColor = 'grey';
+  }
+}
+
+function efeitoListaCinza() {
+  const listaTarefa = document.querySelector('#lista-tarefas');
+  listaTarefa.addEventListener('click', listaCinza);
+}
+
 efeitoBotaoAdicinar();
+
+efeitoListaCinza();
