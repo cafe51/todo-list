af44d3357c8785a645c8d5d5892cc097c7f8c440
cafe51 <cafecafe51@hotmail.com>
2022-02-21 02:15:23 -0400

    requisito 12 resolvido e adicionado botao que carrega lista salva

--- script.js
+++ script.js
@@ -14,12 +14,16 @@ function efeitoBotaoAdicinar() {
   botaoAdcionar.addEventListener('click', adcionarTarefas);
 }
 
-function listaCinza(evt) {
-  const alvo = evt.target;
+function removeDestacados() {
   const itens = listaTarefa.children;
   for (let i = 0; i < itens.length; i += 1) {
     itens[i].classList.remove('destacado');
   }
+}
+
+function listaCinza(evt) {
+  const alvo = evt.target;
+  removeDestacados();
   if (alvo.classList[0] === 'tarefa') {
     alvo.classList.add('destacado');
   }
@@ -120,6 +124,42 @@ function botaoDesceEvento() {
   botaoSobeSelecionado.addEventListener('click', desceSelecionado);
 }
 
+function carregaListaSalva() {
+  apagaTudo();
+  const listaSalvaText = [];
+  const listaSalvaClass = [];
+  for (let i = 0; i < localStorage.length / 2; i += 1) {
+    listaSalvaText[i] = localStorage.getItem(`listaText${i}`);
+    listaSalvaClass[i] = localStorage.getItem(`listaClass${i}`);
+    if (listaSalvaText[i] !== '[object HTMLOListElement]') {
+      const li = document.createElement('li');
+      li.className = listaSalvaClass[i];
+      li.innerText = listaSalvaText[i];
+      listaTarefa.appendChild(li);
+    }
+  }
+  removeDestacados();
+}
+
+function addListToLocalStorage() {
+  const listaPraSalvar = listaTarefa.children;
+  localStorage.clear();
+  for (let i = 0; i < listaPraSalvar.length; i += 1) {
+    localStorage.setItem(`listaText${i}`, listaTarefa.children[i].innerText);
+    localStorage.setItem(`listaClass${i}`, listaTarefa.children[i].className);
+  }
+}
+
+function botaoSalvaEvento() {
+  const botaoSalva = document.querySelector('#salvar-tarefas');
+  botaoSalva.addEventListener('click', addListToLocalStorage);
+}
+
+function botaoCarregaEvento() {
+  const botaoSalva = document.querySelector('#carrega-tarefas');
+  botaoSalva.addEventListener('click', carregaListaSalva);
+}
+
 efeitoBotaoAdicinar();
 
 efeitoListaCinza();
@@ -136,8 +176,8 @@ botaoSobeEvento();
 
 botaoDesceEvento();
 
-// .completed
+carregaListaSalva();
 
-// const tarefa = document.querySelector('.tarefa');
-// tarefa.setAttribute('class', 'completed')
-// tarefa.classList.add('completed');
+botaoSalvaEvento();
+
+botaoCarregaEvento();
